de2dfa85679a99cd46c1497cd858214730004d92
DenisBode <Denis.Bode@gmx.de>
2026-04-20 21:56:24 +0200

    add ai designed logo

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const images = [
+    './img/aperol_spritz.webp',
+    './img/blue_lagoon.webp',
+    './img/cuba_libre.webp',
+    './img/gin_tonic.webp',
+    './img/tequila_sunrise.webp',
+    './img/vodka_tonic.webp'
+];
+
+const gallery = document.getElementById('gallery');
+const popup = document.querySelector('#imagePopup');
+const popupImg = document.querySelector('#popupImg');
+const closeBtn = document.querySelector('#closeBtn');
+
+function render() {
+    gallery.innerHTML = '';
+
+    images.forEach((url) => {
+        const fig = document.createElement('figure');
+        fig.innerHTML = `<img src="${url}" alt="Cocktail">`;
+
+        fig.onclick = () => {
+            popupImg.src = url;
+            popup.showModal();
+        };
+
+        gallery.appendChild(fig);
+    });
+}
+
+closeBtn.onclick = () => popup.close();
+
+render();
+
+
+
+let currentIndex = 0;
+
+function render() {
+    gallery.innerHTML = '';
+    images.forEach((url, index) => {
+        const fig = document.createElement('figure');
+        fig.innerHTML = `<img src="${url}">`;
+        fig.onclick = () => {
+            currentIndex = index;
+            openPopup();
+        };
+        gallery.appendChild(fig);
+    });
+}
+
+function openPopup() {
+    popupImg.src = images[currentIndex];
+    popup.showModal();
+}
+
+
+document.getElementById('nextBtn').onclick = () => {
+    currentIndex = (currentIndex + 1) % images.length;
+    popupImg.src = images[currentIndex];
+};
+
+document.getElementById('prevBtn').onclick = () => {
+    currentIndex = (currentIndex - 1 + images.length) % images.length;
+    popupImg.src = images[currentIndex];
+};
